5ecd65f4b40e4bb643094112f5b74d2603e7f52a
chidiugonna <chidi_ugonna@hotmail.com>
2019-04-09 09:22:17 -0700

    rehashed history because of large files

--- atlas/customAtlas/createCustomAtlas.sh
+++ atlas/customAtlas/createCustomAtlas.sh
@@ -6,15 +6,12 @@
 #subcortical references
 #https://www.lead-dbs.org/helpsupport/knowledge-base/atlasesresources/atlases/
 
-
-
-#download HCP cortical parcellation from schaeffer
+#download schaeffer from schaeffer
 cd atlasSource/schaeffer2018
 wget https://raw.githubusercontent.com/ThomasYeoLab/CBIG/master/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/MNI/Schaefer2018_100Parcels_7Networks_order_FSLMNI152_2mm.nii.gz
 wget https://raw.githubusercontent.com/ThomasYeoLab/CBIG/master/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/MNI/Schaefer2018_100Parcels_7Networks_order.txt
 wget https://raw.githubusercontent.com/ThomasYeoLab/CBIG/master/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/MNI/Schaefer2018_100Parcels_7Networks_order.lut
 
-#manually download DISTAL atlas from https://www.lead-dbs.org/helpsupport/knowledge-base/atlasesresources/atlases
 #manually download FSL Harvard SubCORT
 cd atlasSource/HarvardSC
 cp /home/chidi/share/fsl-6/data/atlases/HarvardOxford/HarvardOxford-sub-maxprob-thr0-1mm.nii.gz .
